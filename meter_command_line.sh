d544cdc38cab6e28098e4780d3cea1ba3c2b4efe
gaolichuang <gaolichuang@gmail.com>
2016-02-14 17:26:28 +0800

    add meteor cmd line

---
+++ meter_command_line.sh
@@ -0,0 +1,11 @@
+# Meteor Command Line
+
+#### install 1.2.1
+> avoid download packages. use http://pan.baidu.com/s/1skc6bLj
+
+#### version
+meteor --version
+
+#### Create Sample
+meteor create --example todos
+meteor create --example localmarket
